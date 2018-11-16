6e94b12cea11edf3c83162469e910b45d9918a80
Maxim Strukov <mxmstrv@gmail.com>
2018-11-15 18:31:57 -0800

    created config and service for saving json files, generated sql queries for fetching the data and saving to json

--- bundle/Command/MigrateCommand.php
+++ bundle/Command/MigrateCommand.php
@@ -14,12 +14,13 @@ declare(strict_types=1);
 
 namespace Novactive\Bundle\FormBuilderBundle\Command;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
+use Novactive\Bundle\eZFormBuilderBundle\Core\IOService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\FetchMode;
 
 /**
  * Class MigrateCommand.
@@ -36,17 +37,21 @@ class MigrateCommand extends Command
      */
     private $connection;
 
+    /**
+     * @var IOService
+     */
+    private $ioService;
+
     public const QUESTION_TYPES = ['EmailEntry', 'TextEntry', 'NumberEntry', 'Paragraph', 'MultipleChoice'];
 
     /**
      * MigrateCommand constructor.
-     *
-     * @param Connection $connection
      */
-    public function __construct(Connection $connection)
+    public function __construct(Connection $connection, IOService $ioService)
     {
         parent::__construct();
         $this->connection = $connection;
+        $this->ioService  = $ioService;
     }
 
     protected function configure(): void
@@ -64,19 +69,19 @@ class MigrateCommand extends Command
         $forms = [];
 
         $surveys = $this->connection->query(
-            'SELECT max(id) as surveyId FROM ezsurvey GROUP BY contentobject_id ORDER BY surveyId'
-        )->fetchAll(FetchMode::COLUMN);
-        foreach ($surveys as $surveyId) {
+            'SELECT max(id) as surveyId, contentobject_id FROM ezsurvey GROUP BY contentobject_id ORDER BY surveyId'
+        )->fetchAll();
+        foreach ($surveys as $survey) {
             $fields = [];
-            $stmt   = $this->connection->prepare(
-                "SELECT * FROM ezsurveyquestion WHERE survey_id = ? AND `type` IN ('".
-                implode("','", self::QUESTION_TYPES)."') ORDER BY tab_order"
-            );
-            $stmt->bindValue(1, $surveyId);
-            $stmt->execute();
-            $questions = $stmt->fetchAll();
+
+            $sql = "SELECT * FROM ezsurveyquestion WHERE survey_id = ? 
+                    AND `type` IN ('".implode("','", self::QUESTION_TYPES)."') ORDER BY tab_order";
+
+            $questions = $this->runQuery($sql, [$survey['surveyId']]);
             foreach ($questions as $question) {
-                //dump($question);
+                if (empty($question['text'])) {
+                    continue;
+                }
                 switch ($question['type']) {
                     case 'EmailEntry':
                         $type = 'Email';
@@ -96,16 +101,81 @@ class MigrateCommand extends Command
                     default:
                         $type = '';
                 }
+                $options = [];
+                if ('Choice' === $type) {
+                    $xml     = simplexml_load_string($question['text2']);
+                    $choices = [];
+                    $counter = 0;
+                    foreach ($xml as $option) {
+                        ++$counter;
+                        $choices[$counter] = ['value' => (string) $option->value, 'weight' => $counter];
+                    }
+                    switch ($question['num']) {
+                        case 1:
+                            $choiceType = 'radio';
+                            break;
+                        case 2:
+                            $choiceType = 'radio';
+                            break;
+                        case 3:
+                            $choiceType = 'checkboxes';
+                            break;
+                        case 4:
+                            $choiceType = 'checkboxes';
+                            break;
+                        case 5:
+                            $choiceType = 'dropdown';
+                            break;
+                        default:
+                            $choiceType = 'dropdown';
+                    }
+                    $options = ['choice_type' => $choiceType, 'choices' => $choices];
+                }
+
                 $fields[] = [
                     'name'   => $question['text'], 'required' => $question['mandatory'],
-                    'weight' => $question['tab_order'], 'options' => [], 'type' => $type
+                    'weight' => $question['tab_order'], 'options' => $options, 'type' => $type,
                 ];
-                break;
             }
-            $forms[] = ['name' => 'Form_'.$surveyId, 'maxSubmissions' => 0, 'fields' => $fields, 'submissions' => []];
+            $form    = ['name' => 'Form_'.$survey['surveyId'], 'maxSubmissions' => 0, 'fields' => $fields];
+            $fileId  = $this->ioService->saveFile($form['name'], json_encode($form));
+            $forms[] = $fileId;
+
+            // Get the Survey Results
+            $sql = 'SELECT sqr.result_id,sqr.question_id,sq.text,sqr.text answer, ';
+
+            $sql .= "CONCAT(sqr.result_id,'-',sqr.question_id) ids, ";
+            $sql .= 'COUNT(CONCAT(sqr.result_id,sqr.question_id)) answersCount ';
+            $sql .= 'FROM ezsurvey s ';
+            $sql .= 'JOIN ezsurveyresult sr ON s.id = sr.survey_id ';
+            $sql .= 'JOIN ezsurveyquestionresult sqr ON sr.id = sqr.result_id ';
+            $sql .= 'JOIN ezsurveyquestion sq ON sqr.question_id = sq.id ';
+            $sql .= 'WHERE s.contentobject_id = ? ';
+            $sql .= 'GROUP BY ids ';
+            $sql .= 'ORDER BY sqr.result_id,sqr.question_id';
+
+            $results = $this->runQuery($sql, [$survey['contentobject_id']]);
+            foreach ($results as $result) {
+                $answers = $result['answer'];
+                if ($result['answersCount'] > 1) {
+                    $sql     = 'SELECT text FROM ezsurveyquestionresult WHERE result_id = ? AND question_id = ?';
+                    $answers = $this->runQuery($sql, [$result['result_id'], $result['question_id']], FetchMode::COLUMN);
+                }
+            }
         }
-        file_put_contents('dump.json', json_encode($forms));
+        $this->ioService->saveFile('manifest', json_encode($forms));
 
         $io->success('Done.');
+    }
+
+    private function runQuery(string $sql, array $parameters = [], $fetchMode = null): array
+    {
+        $stmt = $this->connection->prepare($sql);
+        for ($i = 1, $iMax = count($parameters); $i <= $iMax; ++$i) {
+            $stmt->bindValue($i, $parameters[$i - 1]);
+        }
+        $stmt->execute();
+
+        return $stmt->fetchAll($fetchMode);
     }
 }
