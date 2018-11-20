8bb44efe1ab0b39f9a8956ea7439649fac8eae88
Maxim Strukov <mxmstrv@gmail.com>
2018-11-19 18:19:42 -0800

    fixed fields sorting on editing form on Content Edit page popup #62659

--- ezbundle/Resources/public/js/form-builder.js
+++ ezbundle/Resources/public/js/form-builder.js
@@ -59,6 +59,8 @@
             });
 
             inputNumberEvents();
+
+            fieldSorting();
         });
     }
 
@@ -87,6 +89,30 @@
             } else {
                 $maxSubmissions.removeClass('bg-grey');
             }
+        });
+    }
+
+    function fieldSorting() {
+        var weightSelector = '[name$="weight]"]';
+        var $fieldCollection = $('.js-form-fields-collection-entries');
+        var $document = $(document);
+
+        $document.on('form-builder:field-added', function () {
+            setWeights($('.js-form-field-row'), weightSelector);
+        });
+
+        $fieldCollection.sortable({
+            update: function (event, ui) {
+                setWeights($('.js-form-field-row'), weightSelector);
+            },
+            placeholder: 'formbuilder__sort-placeholder',
+            forcePlaceholderSize: true
+        });
+    }
+
+    function setWeights($elems, weightSelector) {
+        $elems.each(function (index) {
+            $(this).find(weightSelector).val(index);
         });
     }
 
