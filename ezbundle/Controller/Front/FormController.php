9d5c061b3a1ccdd74df222148ade6c42db0db3ae
flovntp <flovntp@gmail.com>
2019-08-01 01:40:30 +0200

    Update FormController.php

--- ezbundle/Controller/Front/FormController.php
+++ ezbundle/Controller/Front/FormController.php
@@ -40,7 +40,7 @@ class FormController
      * Render & handle clientside form on Front Office.
      *
      * @Route("/showfront/{id}", name="novaezformbuilder_front_show_form")
-     * @Template("@ezdesign/fields/ezcustomform_show_front.html.twig")
+     * @Template("NovaeZFormBuilderBundle:fields:ezcustomform_show_front.html.twig")
      */
     public function show(
         Form $formEntity,
