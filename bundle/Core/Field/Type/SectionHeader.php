c57836beae7ae49c02757900e96684e2a3dafe09
Flo HUCK <flovntp@gmail.com>
2019-08-19 18:12:53 +0200

    adding sectionHeader treatment

---
+++ bundle/Core/Field/Type/SectionHeader.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * NovaFormBuilder Bundle.
+ *
+ * @package   Novactive\Bundle\FormBuilderBundle
+ *
+ * @author    Novactive <s.morel@novactive.com>
+ * @copyright 2018 Novactive
+ * @license   https://github.com/Novactive/NovaFormBuilderBundle/blob/master/LICENSE MIT Licence
+ */
+
+declare(strict_types=1);
+
+namespace Novactive\Bundle\FormBuilderBundle\Core\Field\Type;
+
+use Novactive\Bundle\FormBuilderBundle\Core\Field\FieldType;
+use Novactive\Bundle\FormBuilderBundle\Entity\Field;
+use Novactive\Bundle\FormBuilderBundle\Form\Type\ParagraphType;
+use Novactive\Bundle\FormBuilderBundle\Form\Type\SectionHeaderType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+
+class SectionHeader extends FieldType
+{
+    public function getEntityClass(): string
+    {
+        return Field\SectionHeader::class;
+    }
+
+    /**
+     * @param Field\SectionHeader $field
+     */
+    public function mapFieldEditForm(FormInterface $fieldForm, Field $field): void
+    {
+        $fieldForm
+            ->add(
+                'value',
+                TextType::class,
+                [
+                    'required'   => false,
+                    'label'      => 'field.section.data',
+                    'attr'       => ['rows' => 10],
+                    'data'       => $field->getValue()
+
+                ]
+            );
+    }
+
+    /**
+     * @param Field\SectionHeader $field
+     */
+    public function mapFieldCollectForm(FormInterface $fieldForm, Field $field): void
+    {
+        $fieldForm
+            ->add(
+                'value',
+                SectionHeaderType::class,
+                [
+                    'label'       => $field->getValue(),
+                    'data'      => $field->getOption('data')
+                ]
+            );
+    }
+}
---
+++ bundle/Entity/Field/SectionHeader.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * NovaFormBuilder Bundle.
+ *
+ * @package   Novactive\Bundle\FormBuilderBundle
+ *
+ * @author    Novactive <s.morel@novactive.com>
+ * @author    Novactive <f.alexandre@novactive.com>
+ * @copyright 2018 Novactive
+ * @license   https://github.com/Novactive/NovaFormBuilderBundle/blob/master/LICENSE MIT Licence
+ */
+
+declare(strict_types=1);
+
+namespace Novactive\Bundle\FormBuilderBundle\Entity\Field;
+
+use Doctrine\ORM\Mapping as ORM;
+use Novactive\Bundle\FormBuilderBundle\Entity\Field;
+
+/**
+ * @ORM\Entity
+ */
+class SectionHeader extends Field
+{
+    public function getValue()
+    {
+        if (null !== $this->getOption('value')) {
+            return $this->getOption('value');
+        }
+
+        return null;
+    }
+
+    public function setValue($value): Field
+    {
+        $this->setOption('value', $value);
+
+        return $this;
+    }
+}
